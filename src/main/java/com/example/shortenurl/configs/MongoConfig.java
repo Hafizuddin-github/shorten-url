8eef525715240286b1caa2086ea80ad599f19078
nur.hafizuddin <nur.hafizuddin@zillearn.com>
2022-02-26 10:12:40 +0800

    feat: [BE-1] - Added mongoDB connection

---
+++ src/main/java/com/example/shortenurl/configs/MongoConfig.java
@@ -0,0 +1,36 @@
+package com.example.shortenurl.configs;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "dbName";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString =
+                new ConnectionString("mongodb+srv://hafiz:haYNLfnVUKSF4uC4@shorten-url.5pw8b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Override
+    public Collection getMappingBasePackages() {
+        return Collections.singleton("com.example");
+    }
+}
